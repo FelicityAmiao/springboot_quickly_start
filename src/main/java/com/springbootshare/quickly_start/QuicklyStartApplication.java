f8468138911a03075ba5b5c26437aa3f462c8cba
CHENFE2 <felicity.chen@cargosmart.com>
2021-07-19 16:21:49 +0800

    init springboot project

---
+++ src/main/java/com/springbootshare/quickly_start/QuicklyStartApplication.java
@@ -0,0 +1,13 @@
+package com.springbootshare.quickly_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicklyStartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QuicklyStartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/springbootshare/quickly_start/QuicklyStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootshare.quickly_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuicklyStartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
